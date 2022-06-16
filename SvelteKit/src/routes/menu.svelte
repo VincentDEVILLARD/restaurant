9e9371a90ef56fcafd5d832a047c4630b32807a8
margot36 <106960903+margot36@users.noreply.github.com>
2022-06-16 19:40:24 +0200

    Add files via upload

--- SvelteKit/src/routes/menu.svelte
+++ SvelteKit/src/routes/menu.svelte
@@ -14,23 +14,122 @@
 	export const prerender = true;
 </script>
 
+
 <svelte:head>
-	<title>Notre Restaurant</title>
-	<meta name="description" content="À propos de notre restaurant" />
+    <title>Notre Restaurant</title>
+    <meta name="description" content="À propos de notre restaurant" />
+    <style type="text/css">
+        @import url(style.css);
+    </style>
 </svelte:head>
 
 <div class="main-menu">
-	<div class="menu-header" style="background-image:url(https://www.auteuil-brasserie.com/i/auteuil-brasserie/3/4/1/1/8/1/1/5/0/9/9/3/6/1601022541_185/5c3f765351ab6417829beac6be8a47d9.website_hd.jpg)">
-			<div class="row page-header__content narrow">
-				<article class="col-full">
-					<h1 class="menu-header_title">Cartes & Menus</h1>
-				</article>
+    <div class="menu-header">
+        <h1 class="menu-header_title">Cartes & Menus</h1>
+    </div>
+
+    <div class="menu-category">
+		<h2 class="menu-category_brunch">Brunch</h2><p class="menu-category_brunch"> (10h-15h) </p>
+		<h2 class="menu-category_tapas">Tapas</h2><p class="menu-category_brunch"> (18h-00h) </p>
+		<h2 class="menu-category_boissons">Carte des boissons</h2>
+    </div>
+
+<div class="menu-list">
+    <!--Ajouter scroll navigation pour pas de défilement-->
+
+	<h2 class="menu-list_title">Brunch (10h-15h)</h2>
+		<h3 class="menu-list_subcategory">Salé</h3>	
+		<div>
+		    <!-- Liste menu brunch salé - petites faims -->
+			<h4 class="menu-list_subcategory2">Salé - petites faims</h4>
+			<div>
+                <p><span class="gauche" style="font-weight: bold">Titre 1</span><span class="droite" style="font-style:italic;color:grey">Prix 1</span></p><br/>
+                <p>Description 1</p><br/>
+				<br/>
+                <p><span class="gauche" style="font-weight: bold">Titre 2</span><span class="droite" style="font-style:italic;color:grey">Prix 2</span></p><br/>
+                <p>Description 2</p><br/>
 			</div>
+				<!--Liste menu brunch salé - grandes faims-->
+				<h4 class="menu-list_subcategory2">Salé - grandes faims</h4>
+			<div>
+                <p><span class="gauche" style="font-weight: bold">Titre 1</span><span class="droite" style="font-style:italic;color:grey">Prix 1</span></p><br/>
+                <p>Description 1</p><br/>
+				<br/>
+                <p><span class="gauche" style="font-weight: bold">Titre 2</span><span class="droite" style="font-style:italic;color:grey">Prix 2</span></p><br/>
+                <p>Description 2</p><br/>
+			</div>
+		</div>
+		<h3 class="menu-list_subcategory">Sucré</h3>
+		<div>
+			<!--Liste menu brunch sucré-->
+			<h4 class="menu-list_subcategory2">Sucré</h4>
+			<div>
+                <p><span class="gauche" style="font-weight: bold">Titre 1</span><span class="droite" style="font-style:italic;color:grey">Prix 1</span></p><br/>
+                <p>Description 1</p><br/>
+				<br/>
+                <p><span class="gauche" style="font-weight: bold">Titre 2</span><span class="droite" style="font-style:italic;color:grey">Prix 2</span></p><br/>
+                <p>Description 2</p><br/><br/>
+			</div>
+		</div>
+
+	<h2 class="menu-list_title">Tapas (18h-00h)</h2>
+		<!--Liste menu tapas - plateaux découvertes-->
+		<div>
+			<h3 class="menu-list_subcategory">Plateaux Découvertes (2 personnes)</h3>	
+			<div>
+                <p><span class="gauche" style="font-weight: bold">Titre 1</span><span class="droite" style="font-style:italic;color:grey">Prix 1</span></p><br/>
+                <p>Description 1</p><br/>
+				<br/>
+                <p><span class="gauche" style="font-weight: bold">Titre 2</span><span class="droite" style="font-style:italic;color:grey">Prix 2</span></p><br/>
+                <p>Description 2</p><br/>
+			</div>
+		</div>
+		<!--Liste menu tapas - à partager-->
+		<div>
+			<h3 class="menu-list_subcategory">À Partager / Petites Portions</h3>	
+			<div>
+                <p><span class="gauche" style="font-weight: bold">Titre 1</span><span class="droite" style="font-style:italic;color:grey">Prix 1</span></p><br/>
+                <p>Description 1</p><br/>
+				<br/>
+                <p><span class="gauche" style="font-weight: bold">Titre 2</span><span class="droite" style="font-style:italic;color:grey">Prix 2</span></p><br/>
+                <p>Description 2</p><br/>
+			</div>
+		</div>
+		<!--Liste menu tapas - desserts-->
+		<div>
+			<h3 class="menu-list_subcategory">Desserts</h3>	
+            <p><span class="gauche" style="font-weight: bold">Titre 1</span><span class="droite" style="font-style:italic;color:grey">Prix 1</span></p><br/>
+            <p>Description 1</p><br/>
+            <br/>
+            <p><span class="gauche" style="font-weight: bold">Titre 2</span><span class="droite" style="font-style:italic;color:grey">Prix 2</span></p><br/>
+            <p>Description 2</p><br/><br/>
+		</div>
+        
+	<h2 class="menu-list_title">Carte des boissons</h2>
+		<!--Liste menu carte des boissons - sans alcool -->
+		<div>
+			<h3 class="menu-list_subcategory">Sans Alcool</h3>	
+			<div>
+                <p><span class="gauche" style="font-weight: bold">Titre 1</span><span class="droite" style="font-style:italic;color:grey">Prix 1</span></p><br/><br/>
+
+                <p><span class="gauche" style="font-weight: bold">Titre 2</span><span class="droite" style="font-style:italic;color:grey">Prix 2</span></p><br/><br/>
+			</div>
+		</div>
+		<!--Liste menu alcool-->
+		<div>
+			<h3 class="menu-list_subcategory">Alcool</h3>	
+			<div>
+                <p><span class="gauche" style="font-weight: bold">Titre 1</span><span class="droite" style="font-style:italic;color:grey">Prix 1</span></p><br/><br/>
+                <p><span class="gauche" style="font-weight: bold">Titre 2</span><span class="droite" style="font-style:italic;color:grey">Prix 2</span></p><br/><br/>
+			</div>
+		</div>
+		<!--Liste menu boissons chaudes-->
+		<div>
+			<h3 class="menu-list_subcategory">Boissons Chaudes</h3>	
+            <p><span class="gauche" style="font-weight: bold">Titre 1</span><span class="droite" style="font-style:italic;color:grey">Prix 1</span></p><br/><br/>
+            <p><span class="gauche" style="font-weight: bold">Titre 2</span><span class="droite" style="font-style:italic;color:grey">Prix 2</span></p><br/><br/>
+		</div>
 	</div>
 
-	<div class="menu-category">
-	<h2> Brunch </h2><p> (10h-15h) </p>
-	<h2> Tapas </h2><p> (18h-00h) </p>
-	<h2> Carte des boissons </h2>
-	</div>
+</div>
 </div>
