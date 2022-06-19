db3a63c3822b922fddc4a6712a05f9ab80d00522
VincentVega98 <vdevil@hotmail.fr>
2022-06-19 12:14:05 +0200

    reservation + update database

--- SvelteKit/src/routes/database.svelte
+++ SvelteKit/src/routes/database.svelte
@@ -14,13 +14,24 @@
 		
 		return data
 	}
+
+	async function getData2() {
+		const { data, error } = await supabase
+			.from('Reservation')
+			.select()  
+			
+		if (error) throw new Error(error.message)
+		
+		return data
+	}
 </script>
 
 <main>
 	<body>
-	<div>
 
-		<h1>Applications</h1>
+	<div class="row">
+		<div class="column">
+			<h1>Applications</h1>
 		
 		{#await getData()}
 			<p>Fetching data...</p>
@@ -49,19 +60,50 @@
 			<p>Something went wrong while fetching the data:</p>
 			<pre>{error}</pre>
 		{/await}
+		</div>
 
+		<div class="column">
+			<h1>Reservations</h1>
+		
+			{#await getData2()}
+				<p>Fetching data...</p>
+				{:then data}
+				{#each data as cand}
+					<h2>Reservation for{cand.date} at {cand.time}</h2>
+					<p>Number of guests</p>
+					<li>{cand.number}</li>
+					<li>{cand.message}</li>
+					<hr>
+				{/each}
+				{:catch error}
+				<p>Something went wrong while fetching the data:</p>
+				<pre>{error}</pre>
+			{/await}
+		</div>
+	</div> 
 
-	</div>
     </body>
 </main>
 
 
 <style>
-		main {
+
+	main {
         margin: 40px;
 	}
 
 	h2 {
 		font-size:x-large;
+	}
+
+	.column {
+		float: left;
+		width: 50%;
+	}
+
+	.row:after {
+		content: "";
+		display: table;
+		clear: both;
 	}
 </style>
